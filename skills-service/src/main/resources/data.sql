e6321973cd5f9598ac515f4998d9fafc1dce0ed8
Anitha <anithamedikonda19@gmail.com>
2021-05-08 12:55:46 +0530

    handson

---
+++ skills-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into skill (id,name,description,level) values(101L, 'A','C','Beginner');
+insert into skill (id,name,description,level) values(102L, 'B','HTML','Intermediate');
+insert into skill (id,name,description,level) values(103L, 'C','java','Advancce');
+insert into skill (id,name,description,level) values(104L, 'D','Spring MVC','Advance');
+insert into skill (id,name,description,level) values(105L, 'E','Jquery','Intermediate');
